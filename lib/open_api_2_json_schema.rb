00c6f78dfbba8e2ec3d1b410f7d91ec5bdf45024
hieuk09 <hieuk09@gmail.com>
2022-03-26 18:25:22 +0800

    Initial commit

---
+++ lib/open_api_2_json_schema.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "open_api_2_json_schema/version"
+
+module OpenApi2JsonSchema
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/open_api_2_json_schema_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe OpenApi2JsonSchema do
+  it "has a version number" do
+    expect(OpenApi2JsonSchema::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
