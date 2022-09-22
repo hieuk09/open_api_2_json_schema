0b1c483460c275ab5221c6dd93de5f3ff9c80d5e
DiHuynh35 <di.huynh@kaligo.com>
2022-09-22 10:27:33 +0700

    Add schema inheritance support feature

---
+++ spec/attribute_handlers/all_of_spec.rb
@@ -0,0 +1,60 @@
+RSpec.describe OpenApi2JsonSchema::AttributeHandlers::AllOf do
+  subject { described_class.new.call(all_of_schemas) }
+
+  context 'when the all_of_schemas is invalid' do
+    let(:all_of_schemas) { {} }
+
+    it { expect { subject }.to raise_error('allOf schema must be an Array') }
+  end
+
+  context 'when there is invalid sub-schema' do
+    let(:all_of_schemas) { [1] }
+
+    it { expect { subject }.to raise_error('Invalid schema: allOf[0]') }
+  end
+
+  context 'when the everything is valid' do
+    let(:all_of_schemas) do
+      [
+        {
+          '$ref' => '/path-to-file#/schema'
+        },
+        {
+          'type' => 'object',
+          'properties' => {
+            'attr_1' => {
+              'type' => 'string'
+            },
+            'attr_2' => {
+              'type' => 'string'
+            }
+          }
+        }
+      ]
+    end
+    let(:expected_result) do
+      [
+        { 'type' => 'object' },
+        {
+          'type' => 'object',
+          'properties' => {
+            'attr_1' => {
+              'type' => 'string'
+            },
+            'attr_2' => {
+              'type' => 'string'
+            }
+          }
+        }
+      ]
+    end
+
+    before do
+      expect(OpenApi2JsonSchema::RefSchemaParser).to receive_message_chain(:new, :call)
+        .with('/path-to-file#/schema')
+        .and_return({ 'type' => 'object' })
+    end
+
+    it { expect(subject).to eq(expected_result) }
+  end
+end
