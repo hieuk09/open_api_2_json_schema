342666bf809bece4bc630a64455f60f009e5bfbc
Karisse Khoo <karissekhoo@hotmail.com>
2022-10-18 15:38:43 +0800

    add specs

--- spec/attribute_handlers/discriminator_spec.rb
+++ spec/attribute_handlers/discriminator_spec.rb
@@ -1,6 +1,22 @@
 RSpec.describe OpenApi2JsonSchema::AttributeHandlers::Discriminator do
   subject { described_class.new.call(discriminator_schema) }
 
+  context 'when there is invalid sub-schema' do
+
+    context 'when schema is not a Hash' do
+      let(:discriminator_schema) { [1] }
+
+      it { expect { subject }.to raise_error('discriminator schema must be a Hash') }
+    end
+
+    context 'when schema is not a Hash' do
+      let(:discriminator_schema) { {} }
+
+      it { expect { subject }.to raise_error('discriminator schema must contain mapping') }
+    end
+
+  end
+
   context "when schema is valid" do
     let(:discriminator_schema) do
       {
